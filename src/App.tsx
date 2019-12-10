8bb9c65c9243431571d4c3d5e6526263be3487f0
geusan <dnay2k@taggers.io>
2019-12-10 15:00:13 +0900

    Move vue to tsx

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Component, Vue } from 'vue-property-decorator';
+import './assets/stylus/pages/App.styl';
+import { HelloWorld } from './components/HelloWorld';
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+})
+export default class App extends Vue {
+  public render() {
+    console.log(HelloWorld);
+    return (
+      <div id={'app'}>
+        <img alt={'Vue logo'} src='/logo.png'/>
+        <HelloWorld />
+      </div>
+    );
+  }
+}
